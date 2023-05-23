3fba0bda5ee2af309e067a52a6f8147dce0bdc66
JungInLee0130 <dlwjddls0130@naver.com>
2023-05-23 16:27:15 +0900

    fix: refreshtoken 백엔드 전송

--- src/components/Member/MemberList.vue
+++ src/components/Member/MemberList.vue
@@ -36,10 +36,9 @@
 </template>
 <script>
 import MemberItem from "@/components/Member/MemberItem.vue";
-import { apiInstance } from '@/api/lib/index';
+import http from '@/api/lib/axios';
 import {mapActions} from 'vuex';
 const memberStore = "memberStore";
-const http = apiInstance();
 export default {
     name: "MemberList",
     components: {
@@ -72,6 +71,7 @@ export default {
                     console.log(error);
                     if (this.$store.state.memberStore.isValidToken == false){
                         alert("토큰 만료. 엑세스 토큰 재발급.");
+                        this.$router.push({ name: "home" });
                         this.tokenRegeneration();                          
                     }
                 });
